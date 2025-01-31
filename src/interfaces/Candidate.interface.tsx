798036db21e4e4c3f31ca913f430d87acd7ffd1b
this-is-yosuke <yosuke.kibe@gmail.com>
2025-01-30 20:32:56 -0700

    Updated the candidate interface
    Thought this was included in the last commit

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,7 +1,7 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 export default interface Candidate {
-    readonly Image: string | null;
     readonly Name: string | null;
+    readonly ProfilePic: string | null;
     readonly Location: string | null;
     readonly Email: string | null;
     readonly Company: string | null;
